851819b1acfc04acdcb04cfa4f1db70870e8d077
Mitchell Chung <mchung94@gmail.com>
2019-07-27 20:08:20 -0700

    Make the delivery script more clear
    Use more ASDF functions instead of system-specific things, as well as
    running things from the current directory instead of inside the dist
    directory.

--- delivery-script.lisp
+++ delivery-script.lisp
@@ -1,28 +1,32 @@
 ;;;; LispWorks-specific delivery script
-;;;; Review carefully if using this script on your own system, because it
-;;;; loads the default LispWorks user initialization file and expects a recent
-;;;; version of ASDF and Quicklisp to be loaded by it, and expects the current
-;;;; directory to be the same directory this file is in.
+;;;; Review this script if using this script on your own system.
 
 ;;; Make sure to load all patches before delivery
 (in-package "CL-USER")
 (load-all-patches)
 
-;;; Load the LispWorks user initialization file
+;;; Load the LispWorks user initialization file.
+;;; This file must load a recent version of ASDF and Quicklisp in order to
+;;; load the project successfully.
 (load *init-file-name*)
 
-;;; Delete the dist/ directory to remove any previous builds and recreate it
-(sys:call-system "rd /s /q dist")
-(ensure-directories-exist "dist/")
-(change-directory "dist/")
+;;; Delete the dist/ directory to remove any previous builds
+(uiop:delete-directory-tree (pathname "dist/")
+                            :validate t
+                            :if-does-not-exist :ignore)
 
 ;;; Load the application
-(push "../" asdf:*central-registry*)
+;;; This assumes the current directory contains sudoku-solver.asd
+(push (uiop/os:getcwd) asdf:*central-registry*)
 (asdf:load-system "sudoku-solver")
-(copy-file "../README.md" "README.md")
 
-;;; Deliver the application, sudoku-solver.exe
-(deliver 'sudoku-solver:main "sudoku-solver.exe" 5
+;;; Run the tests (in 1am, failures signal errors so it won't pass this part)
+(asdf:test-system "sudoku-solver")
+
+;;; Deliver the application, sudoku-solver.exe, and its README.md
+(ensure-directories-exist "dist/")
+(copy-file "README.md" "dist/README.md")
+(deliver 'sudoku-solver:main "dist/sudoku-solver.exe" 5
          :interface :capi
-         :icon-file "../solver.ico"
+         :icon-file "solver.ico"
          :startup-bitmap-file nil)
